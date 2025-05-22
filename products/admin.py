b5a4a5775ecb500962d68b1de5bbf9578e66586f
Alex Chernov <nak4442@gmail.com>
2025-05-22 15:51:28 +0300

    Русификация админки

--- products/admin.py
+++ products/admin.py
@@ -4,7 +4,24 @@ from .models import Product, Category
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'category')
-    search_fields = ('name',)
     list_filter = ('category',)
+    search_fields = ('name', 'description')
+    fieldsets = (
+        (('Основная информация'), {
+            'fields': ('name', 'category', 'price')
+        }),
+        (('Дополнительно'), {
+            'fields': ('description', 'image'),
+            'classes': ('collapse',)
+        }),
+    )
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+# Русификация заголовка админки
+admin.site.site_header = ('Администрирование магазина')
+admin.site.site_title = ('Магазин')
+admin.site.index_title = ('Панель управления')
--- products/models.py
+++ products/models.py
@@ -2,9 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name = ('Категория')
+        verbose_name_plural = ('Категории')
+
+    def __str__(self):
+        return self.name
     name = models.CharField(max_length=100)
 
 class Product(models.Model):
+    class Meta:
+        verbose_name = ('Товар')
+        verbose_name_plural = ('Товары')
+
+    def __str__(self):
+        return self.name
+    
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
